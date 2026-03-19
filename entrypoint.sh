cdf9c78843a949e4bd4160a1bbc1d0a841725dc6
Doridian <git@doridian.net>
2026-03-18 17:05:48 -0700

    Stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
-curl -fL -o /models/download.gguf "${LLAMA_MODEL_URL}"
+if [ ! -f /models/download.gguf ]; then
+    echo "Downloading model from ${LLAMA_MODEL_URL}"
+    curl -fL -o /models/download.gguf "${LLAMA_MODEL_URL}"
+else
+    echo 'Model already exists at /models/download.gguf, skipping download.'
+fi
 
 exec llama-server --host :: --port "${LLAMA_PORT:-8080}" --model /models/download.gguf --threads "$(nproc)"
